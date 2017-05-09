a2091daefc488510b6697454b9d8f3537fa7f132
fzy <1010368236@qq.com>
2017-05-09 11:26:51 +0800

    textFiled
    textFiled抖动效果

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextField
+//
+//  Created by fzy on 2017/5/9.
+//  Copyright © 2017年 fzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
